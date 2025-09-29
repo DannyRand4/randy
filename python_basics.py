005f7fd439552c21c8e5f2ec0250e2cadb147637
DannyRand4 <dannychirunga@gmail.com>
2025-09-29 13:38:38 +0300

    Added python basics practice file

---
+++ python_basics.py
@@ -0,0 +1,75 @@
+#variable_name = data
+#name = input("what is your name? ")
+#print(f"hello, {name}!") #f strings
+#age = input("what is your age? ")
+#print(f"hello,{name} your age is :{age}")
+#course_code = 1435 #snake case
+#courseCode = 56735 #camel case
+#foods = ["maize", "rice","beans"]
+
+
+
+
+
+
+
+
+
+
+#myfloat = 4.25
+#print(type(myfloat))
+#name = "daniel"
+#print(type(name))
+#checking the data type using instance 
+#print(isinstance(798, float))
+#print(isinstance("daniel", float))
+#dictionary
+#students = {
+    #"Alice": 21,
+    #"John": 23,
+    #"Musa": 26,
+    #"Freddy": 29
+#}
+#print(type(students))
+#sets
+#age = {7,8,3}
+#checking data type
+#print(type(age))
+#list=[1,2,3,4,5,6]#integer
+#print(type(list))
+list2=["dan","freddy","musa"]
+print(list2)
+list3=[True,False]#bollean
+print(list3)
+list4=["dan",1,23,True]#mixed
+print(list4)
+list5=[]#empty list
+print(list5)
+list5.append(1)#adding items on a list
+print(list5)
+list5.append("danny")#adding items on a list
+print(list5)
+list3.append(1)
+print(list3)
+list4.extend(list3)#adding two lists
+print(list4)
+list4.pop(6)#removes the last items
+print(list4)
+list3.remove(1)#removes any item you want from the list
+print(list3)
+list3.remove(1)
+print(list3)
+list3.remove(0)
+print(list3)
+list5.insert(2,5)#inserting items
+print(list5)
+
+#ACCESSING AND INDEX
+print([list4[4]])
+print(list4[0])
+print(list3[1:4])
+print(list2[1:2])
+print(list2[:3])
+print(list4[:6])
+#.SORT METHOD ASSIGNMENT
+#TUPLES
